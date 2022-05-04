61424663cee82dafc4572f2b75e9b451e49cdc70
jorgefz <jorgefz.fernandez@gmail.com>
2022-05-04 14:17:50 +0100

    Enforce null termination on reading shader source from file

--- lince/src/renderer/shader.c
+++ lince/src/renderer/shader.c
@@ -173,6 +173,7 @@ static char* LinceReadFile(const char* path){
 	LINCE_ASSERT_ALLOC(source, size+1);
 
 	for(size_t i=0; i!=size; ++i) source[i] = (char)fgetc(handle);
+	source[size] = '\0';
 
 	fclose(handle);
 	return source;
