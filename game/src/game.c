039ad4dc5e0361ea22ec7fe9cd0e9e7cd1375e5f
jorgefz <jorgefz.fernandez@gmail.com>
2022-07-19 10:23:24 +0100

    Added tile-based walking & idle animations to test grounds

--- game/src/game.c
+++ game/src/game.c
@@ -12,8 +12,65 @@
 #include "pong.h"
 #include "missile_command.h"
 #include "nk_test.h"
+#include <assert.h>
 
 #include "renderer/tileset.h"
+
+
+typedef struct SpriteAnim {
+	LinceTile* frames;			// array of animation frames
+	uint32_t frame_count;	// number of animation frames
+
+	float frametime;		// duration of each frame in ms
+	float time;				// time counter between frames
+	uint32_t current_frame;	// current active frame to render
+
+	bool loop;				// indicates wether animation loops forever, unused
+} SpriteAnim;
+
+
+SpriteAnim* CreateAnim(LinceTile* frames, uint32_t frame_count, float frametime, uint32_t start_frame, bool loop){
+
+	SpriteAnim* anim = calloc(1, sizeof(SpriteAnim));
+	assert(anim);
+
+	anim->frames = malloc(sizeof(LinceTile) * frame_count);
+	assert(anim->frames);
+	memmove(anim->frames, frames, sizeof(LinceTile) * frame_count);
+
+	anim->frame_count = frame_count;
+	anim->frametime = frametime;
+	anim->time = frametime;
+	anim->current_frame = start_frame;
+	anim->loop = loop;
+
+	return anim;
+}
+
+void UpdateAnim(SpriteAnim* anim, float dt){
+	anim->time -= dt;
+
+	if(anim->time <= 0.0f){
+		anim->current_frame++;
+		if(anim->current_frame >= anim->frame_count){
+			anim->current_frame = 0;
+		}
+		anim->time = anim->frametime;
+	}
+
+}
+
+void ResetAnim(SpriteAnim* anim){
+	anim->time = anim->frametime;
+	anim->current_frame = 0;
+}
+
+
+void DeleteAnim(SpriteAnim* anim){
+	if(!anim) return;
+	if(anim->frames) free(anim->frames);
+	free(anim);
+}
 
 enum TileNames {
     TILE_GRASS = 0,
@@ -23,12 +80,16 @@ enum TileNames {
     TILE_COUNT
 };
 
-enum WalkingTiles {
-    TILE_FRONT,
-    TILE_BACK,
-    TILE_LEFT,
-    TILE_RIGHT,
-    TILE_WALKING_COUNT
+enum WalkingAnims {
+    ANIM_FRONT,
+    ANIM_FRONT_IDLE,
+    ANIM_BACK,
+    ANIM_BACK_IDLE,
+    ANIM_LEFT,
+    ANIM_LEFT_IDLE,
+    ANIM_RIGHT,
+    ANIM_RIGHT_IDLE,
+    ANIM_COUNT
 };
 
 typedef struct MyLayer {
@@ -47,8 +108,8 @@ typedef struct MyLayer {
     LinceTexture* walking_tileset;
 
     LinceTile tiles[TILE_COUNT];
-    LinceTile walking_tiles[TILE_WALKING_COUNT];
-    uint8_t player_facing;
+    SpriteAnim* player_anims[ANIM_COUNT];
+    uint8_t current_anim;
 
     vec4 color;
     LinceCamera* cam;
@@ -76,12 +137,45 @@ void MyLayerOnAttach(LinceLayer* layer) {
     data->tiles[TILE_TREE]    = LinceGetTile(data->tileset, (vec2){9,5}, (vec2){16,16}, (vec2){2, 2});
     data->tiles[TILE_CHICKEN] = LinceGetTile(data->tileset, (vec2){0,1}, (vec2){16,16}, (vec2){1, 1});
 
-    data->walking_tiles[TILE_FRONT] = LinceGetTile(data->walking_tileset, (vec2){1,0}, (vec2){24,24}, (vec2){1,1});
-    data->walking_tiles[TILE_BACK]  = LinceGetTile(data->walking_tileset, (vec2){1,3}, (vec2){24,24}, (vec2){1,1});
-    data->walking_tiles[TILE_LEFT]  = LinceGetTile(data->walking_tileset, (vec2){1,2}, (vec2){24,24}, (vec2){1,1});
-    data->walking_tiles[TILE_RIGHT] = LinceGetTile(data->walking_tileset, (vec2){1,1}, (vec2){24,24}, (vec2){1,1});
+    LinceTile front_walk_tiles[] = {
+        LinceGetTile(data->walking_tileset, (vec2){0,0}, (vec2){24,24}, (vec2){1,1}),
+        LinceGetTile(data->walking_tileset, (vec2){2,0}, (vec2){24,24}, (vec2){1,1}),
+    };
+    LinceTile front_idle_tiles[] = {
+        LinceGetTile(data->walking_tileset, (vec2){1,0}, (vec2){24,24}, (vec2){1,1}),
+    };
+    LinceTile back_walk_tiles[] = {
+        LinceGetTile(data->walking_tileset, (vec2){0,3}, (vec2){24,24}, (vec2){1,1}),
+        LinceGetTile(data->walking_tileset, (vec2){2,3}, (vec2){24,24}, (vec2){1,1}),
+    };
+    LinceTile back_idle_tiles[] = {
+        LinceGetTile(data->walking_tileset, (vec2){1,3}, (vec2){24,24}, (vec2){1,1}),
+    };
+    LinceTile right_walk_tiles[] = {
+        LinceGetTile(data->walking_tileset, (vec2){0,1}, (vec2){24,24}, (vec2){1,1}),
+        LinceGetTile(data->walking_tileset, (vec2){2,1}, (vec2){24,24}, (vec2){1,1}),
+    };
+    LinceTile right_idle_tiles[] = {
+        LinceGetTile(data->walking_tileset, (vec2){1,1}, (vec2){24,24}, (vec2){1,1}),
+    };
+    LinceTile left_walk_tiles[] = {
+        LinceGetTile(data->walking_tileset, (vec2){0,2}, (vec2){24,24}, (vec2){1,1}),
+        LinceGetTile(data->walking_tileset, (vec2){2,2}, (vec2){24,24}, (vec2){1,1}),
+    };
+    LinceTile left_idle_tiles[] = {
+        LinceGetTile(data->walking_tileset, (vec2){1,2}, (vec2){24,24}, (vec2){1,1}),
+    };
 
-    data->player_facing = TILE_FRONT;
+    data->player_anims[ANIM_FRONT]      = CreateAnim(front_walk_tiles, 2, 300.0f, 0, 0);
+    data->player_anims[ANIM_BACK]       = CreateAnim(back_walk_tiles, 2, 300.0f, 0, 0);
+    data->player_anims[ANIM_LEFT]       = CreateAnim(left_walk_tiles, 2, 300.0f, 0, 0);
+    data->player_anims[ANIM_RIGHT]      = CreateAnim(right_walk_tiles, 2, 300.0f, 0, 0);
+    data->player_anims[ANIM_FRONT_IDLE] = CreateAnim(front_idle_tiles, 1, 300.0f, 0, 0);
+    data->player_anims[ANIM_BACK_IDLE]  = CreateAnim(back_idle_tiles, 1, 300.0f, 0, 0);
+    data->player_anims[ANIM_LEFT_IDLE]  = CreateAnim(left_idle_tiles, 1, 300.0f, 0, 0);
+    data->player_anims[ANIM_RIGHT_IDLE] = CreateAnim(right_idle_tiles, 1, 300.0f, 0, 0);
+
+    data->current_anim = ANIM_BACK_IDLE;
     
 }
 
@@ -93,6 +187,11 @@ void MyLayerOnDetach(LinceLayer* layer) {
     LinceDeleteTexture(data->tex_back);
     LinceDeleteTexture(data->walking_tileset);
     LinceDeleteCamera(data->cam);
+
+    for(int i = 0; i != ANIM_COUNT; ++i){
+        DeleteAnim(data->player_anims[i]);
+        data->player_anims[i] = NULL;
+    }
 
     free(data);
 }
@@ -112,22 +211,43 @@ void MyLayerOnUpdate(LinceLayer* layer, float dt) {
     const float dr = cam_speed * dt * zoom;
 
     // camera & player movement
+    uint8_t next_anim = data->current_anim;
+
     if (LinceIsKeyPressed(LinceKey_w)){
         data->cam->pos[1] += dr;
-        data->player_facing = TILE_FRONT;
+        next_anim = ANIM_FRONT;
     }
     if (LinceIsKeyPressed(LinceKey_s)){
         data->cam->pos[1] -= dr;
-        data->player_facing = TILE_BACK;
+        next_anim = ANIM_BACK;
     }
     if (LinceIsKeyPressed(LinceKey_d)){
         data->cam->pos[0] += dr;
-        data->player_facing = TILE_RIGHT;
+        next_anim = ANIM_RIGHT;
     }
     if (LinceIsKeyPressed(LinceKey_a)){
         data->cam->pos[0] -= dr;
-        data->player_facing = TILE_LEFT;
+        next_anim = ANIM_LEFT;
     }
+    if(
+        !LinceIsKeyPressed(LinceKey_w) && 
+        !LinceIsKeyPressed(LinceKey_s) &&
+        !LinceIsKeyPressed(LinceKey_d) &&
+        !LinceIsKeyPressed(LinceKey_a)
+    ){
+        switch(data->current_anim){
+        case ANIM_FRONT: next_anim = ANIM_FRONT_IDLE; break;
+        case ANIM_BACK:  next_anim = ANIM_BACK_IDLE;  break;
+        case ANIM_RIGHT: next_anim = ANIM_RIGHT_IDLE; break;
+        case ANIM_LEFT:  next_anim = ANIM_LEFT_IDLE;  break;
+        default: break;
+        };
+    }
+
+    if(next_anim != data->current_anim){
+        ResetAnim(data->player_anims[data->current_anim]);
+    }
+    data->current_anim = next_anim;
 
     LinceUIText(ui, "DebugFPS", 20, 20, LinceFont_Droid30, 10, "FPS: %.0f", 1000.0/dt);
     LinceUIText(ui, "DebugDT",  20, 42, LinceFont_Droid30, 15, "dt: %.1f ms", dt);
@@ -203,12 +323,24 @@ void MyLayerOnUpdate(LinceLayer* layer, float dt) {
     });
 
     // PLAYER
+    /*
     LinceDrawQuad((LinceQuadProps){
         .x=data->cam->pos[0],
         .y=data->cam->pos[1],
         .w=1.5f, .h=1.5f,
         .color={1,1,1,1},
         .tile = &data->walking_tiles[data->player_facing],
+        .zorder = 0.6
+    });
+    */
+    SpriteAnim* anim = data->player_anims[data->current_anim];
+    UpdateAnim(anim, dt);
+    LinceDrawQuad((LinceQuadProps){
+        .x=data->cam->pos[0],
+        .y=data->cam->pos[1],
+        .w=1.5f, .h=1.5f,
+        .color={1,1,1,1},
+        .tile = &anim->frames[anim->current_frame],
         .zorder = 0.6
     });
     
