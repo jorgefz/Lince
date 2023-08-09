d5f782b1bbad760a4c629bdfc806c173cb723075
jorgefz <jorgefz.fernandez@gmail.com>
2023-08-09 09:36:57 +0100

    Updated required OpenGL version to 4.5 to support glTextureStorage2D

--- lince/src/lince/core/window.c
+++ lince/src/lince/core/window.c
@@ -44,7 +44,7 @@ LinceWindow* LinceCreateWindow(uint32_t width, uint32_t height, const char* titl
     
     /* Using OpenGL 4.0 */
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     glfwSetErrorCallback(GLFWErrorCallback);
