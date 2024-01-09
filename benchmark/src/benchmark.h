3488656310a0759972bd56579803bf8305a5bcdc
jorgefz <jorgefz.fernandez@gmail.com>
2024-01-09 23:12:37 +0000

    Fixed units of benchmark clock to ms

--- benchmark/src/benchmark.h
+++ benchmark/src/benchmark.h
@@ -11,7 +11,7 @@
 
 #define BENCHMARK_LOOP(COUNTER_TYPE, COUNTER, ITER_NUM) { clock_t _##COUNTER##_timer = clock(); COUNTER_TYPE COUNTER; for (COUNTER = 0; COUNTER != ITER_NUM ; ++COUNTER)
 
-#define BENCHMARK_END(COUNTER_TYPE, COUNTER, ITER_NUM) _##COUNTER##_timer = clock() - _##COUNTER##_timer; printf("[BENCHMARK] %ld operations in %ld ns (%.2g op/ns)\n", (long)ITER_NUM, (long)_##COUNTER##_timer, (double)ITER_NUM/(double)_##COUNTER##_timer); }
+#define BENCHMARK_END(COUNTER_TYPE, COUNTER, ITER_NUM) _##COUNTER##_timer = clock() - _##COUNTER##_timer; printf("[BENCHMARK] %ld operations in %ld ms (%.2g op/ms)\n", (long)ITER_NUM, (long)_##COUNTER##_timer, (double)ITER_NUM/(double)_##COUNTER##_timer); }
 
 
 #endif /* LINCE_BENCHMARK_H */
