6c65aa64ca2dd59a5e7c0392166392a3684e166d
jorgefz <jorgefz.fernandez@gmail.com>
2024-02-29 14:22:24 +0000

    Updated demos to follow the tutorials

--- demos/blank/src/main.c
+++ demos/blank/src/main.c
@@ -4,6 +4,11 @@
 
 int main() {
 
+	LinceApp* app = LinceGetApp();
+	app->screen_width = 800;
+	app->screen_height = 500;
+	LinceAppSetTitle("My Window");
+
 	LinceRun();
 
 	return 0;
--- demos/platformer/src/main.c
+++ demos/platformer/src/main.c
@@ -1,49 +1,33 @@
 #include <lince.h>
 
-typedef struct GameState {
-    LinceCamera camera;
-} GameState;
-
 LinceCamera camera;
 LinceSprite square = {
-    .x = 0, .y = 0,     // position
-    .w = 1, .h = 1,     // size
-    .color = {1, 0, 0, 1} // rgba
+    .x = 0, .y = 0,       // position at the centre of the screen
+    .w = 1, .h = 1,       // size in "world coordinates"
+    .color = {1, 0, 0, 1} // rgba, red and fully opaque
 };
 
 void OnInit() {
-	LinceInitCamera(&camera, LinceGetAspectRatio());
-    // LincePushAssetDir(&LinceGetApp()->asset_manager, "../../../lince/lince/assets");
+    LinceInitCamera(&camera, 800.0 / 500.0);
 }
 
 void OnUpdate(float dt) {
-    LinceResizeCameraView(&camera, LinceGetAspectRatio());
-    LinceUpdateCamera(&camera);
-
-    if(LinceIsKeyPressed(LinceKey_Right)) square.x += 0.02f;
-    if(LinceIsKeyPressed(LinceKey_Left))  square.x -= 0.02f;
-    if(LinceIsKeyPressed(LinceKey_Up))    square.y += 0.02f;
-    if(LinceIsKeyPressed(LinceKey_Down))  square.y -= 0.02f;
-
     LinceBeginScene(&camera);
     LinceDrawSprite(&square, NULL);
     LinceEndScene();
 }
 
-void OnTerminate() {
-	
-}
-
-
 int main() {
 
     LinceApp* app = LinceGetApp();
+    app->screen_width = 800;
+    app->screen_height = 500;
+    LinceAppSetTitle("My Window");
+
     app->on_init = OnInit;
     app->on_update = OnUpdate;
-    app->on_terminate = OnTerminate;
-    LinceAppSetTitle("Platformer Demo");
-    
-	LinceRun();
 
-	return 0;
+    LinceRun();
+
+    return 0;
 }
