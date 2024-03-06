00dbd350de7f7d9c8e4df253ccb708e5bf29b3a5
jorgefz <jorgefz.fernandez@gmail.com>
2024-03-06 09:34:32 +0000

    Improved windows bat script to detect if input project does not exist

--- winbuild.bat
+++ winbuild.bat
@@ -3,10 +3,17 @@
 set "proj=%1"
 IF not defined proj GOTO error
 
+set "projfile=build/%proj%/%proj%.vcxproj"
+IF not exist projfile GOTO error2
+
 echo Building project %proj%
 devenv lince.sln /Project build/%proj%/%proj%.vcxproj /Build "Debug|x64"
 exit /b
 
 :error
 echo Error: provide project name
+exit /b
+
+:error2
+echo Error: project '%proj%' does not exist
 exit /b
