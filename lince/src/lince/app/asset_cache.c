bf0d45b1a41ec45eb338c87c19b8ed8fbb353344
jorgefz <jorgefz.fernandez@gmail.com>
2024-03-21 11:19:25 +0000

    Stored length of directory of executable and renamed some internal asset cache members

--- lince/src/lince/app/asset_cache.c
+++ lince/src/lince/app/asset_cache.c
@@ -3,32 +3,32 @@
 #include "lince/core/logger.h"
 
 void LinceInitAssetCache(LinceAssetCache* cache){
-    array_init(&cache->asset_folders, LINCE_PATH_MAX * sizeof(char));
-    LinceFetchExeDir(cache->exe_dir, LINCE_PATH_MAX);
+    array_init(&cache->folders, LINCE_PATH_MAX * sizeof(char));
+    cache->exedir_length = LinceFetchExeDir(cache->exedir, LINCE_PATH_MAX);
 }
 
 void LinceUninitAssetCache(LinceAssetCache* cache){
-    array_uninit(&cache->asset_folders);
+    array_uninit(&cache->folders);
 }
 
 LinceBool LinceAssetCachePushFolder(LinceAssetCache* cache, const char* dir){
     uint32_t length = (uint32_t)strlen(dir);
-    uint32_t exedir_length = (uint32_t)strlen(cache->exe_dir);
+    size_t exe_len = cache->exedir_length + 1;
     
-    LINCE_ASSERT(length < LINCE_PATH_MAX - exedir_length - 1,
+    LINCE_ASSERT(length < LINCE_PATH_MAX - exe_len - 1,
         "Asset directory is too long. Length %u but max is %u",
-        length, LINCE_PATH_MAX-exedir_length-1);
+        length, LINCE_PATH_MAX - exe_len - 1);
     
-    array_push_front(&cache->asset_folders, NULL);
-    char* p = array_front(&cache->asset_folders);
+    array_push_front(&cache->folders, NULL);
+    char* p = array_front(&cache->folders);
 
     // Prepend directory of executable
-    memmove(p, cache->exe_dir, exedir_length);
-    if (p[exedir_length-1] != '\\' && p[exedir_length-1] != '/'){
-        p[exedir_length] = '/';
-        exedir_length++;
+    memmove(p, cache->exedir, exe_len);
+    if (p[exe_len-1] != '\\' && p[exe_len-1] != '/'){
+        p[exe_len] = '/';
+        exe_len++;
     }
-    p += exedir_length;
+    p += exe_len;
 
     // Append relative directory to assets folder
     memmove(p, dir, length);
@@ -38,23 +38,23 @@ LinceBool LinceAssetCachePushFolder(LinceAssetCache* cache, const char* dir){
     }
     p[length] = '\0';
 
-    if(LinceIsDir(array_front(&cache->asset_folders)) != 1){
+    if(LinceIsDir(array_front(&cache->folders)) != 1){
         LINCE_WARN("Failed to add assets folder because it does not exist: '%s'",
-            (char*)array_front(&cache->asset_folders));
-        array_pop_front(&cache->asset_folders);
+            (char*)array_front(&cache->folders));
+        array_pop_front(&cache->folders);
         return LinceFalse;
     }
 
-    LINCE_INFO("Added assets folder '%s'", (char*)array_front(&cache->asset_folders));
+    LINCE_INFO("Added assets folder '%s'", (char*)array_front(&cache->folders));
     return LinceTrue;
 }
 
 
 char* LinceAssetCacheFetchPath(LinceAssetCache* cache, const char* asset_filename){
 
-    array_t* folders = &cache->asset_folders;
+    array_t* folders = &cache->folders;
     for(uint32_t i = 0; i != folders->size; ++i){
-       char* dir = array_get(&cache->asset_folders, i);
+       char* dir = array_get(&cache->folders, i);
         uint32_t dir_len = (uint32_t)strlen(dir);
         if (dir_len + strlen(asset_filename) >= LINCE_PATH_MAX){
             LINCE_WARN("Skipping path, too long: '%s' + '%s'", dir, asset_filename);
@@ -71,3 +71,25 @@ char* LinceAssetCacheFetchPath(LinceAssetCache* cache, const char* asset_filenam
     LINCE_WARN("Could not locate asset '%s'", asset_filename);
     return NULL;
 }
+
+/*
+void LinceAssetCacheLoad(LinceAssetCache* cache, const char* filename, int type){
+
+    // Build full path
+    // path = cache->exedir + asset_folder + filename
+
+    void* handle;
+    char* path;
+
+    switch(type){
+        case LinceAsset_Texture:
+            handle = LinceLoadTexture(path, 0);
+            break;
+        case LinceAsset_Shader:
+            handle = LinceLoadShader(path, headers);
+        case LinceAsset_Font:
+            handle = LinceUILoadFonts(path, sizes);
+    }
+
+}
+*/
--- lince/src/lince/app/asset_cache.h
+++ lince/src/lince/app/asset_cache.h
@@ -3,8 +3,8 @@
 
 #include "lince/core/core.h"
 #include "lince/containers/array.h"
+#include "lince/containers/hashmap.h"
 
-/*
 enum LinceAssetType {
     LinceAsset_None = -1,
     LinceAsset_Image,
@@ -13,48 +13,35 @@ enum LinceAssetType {
     LinceAsset_ShaderHeader,
     LinceAsset_ShaderProgram,
     LinceAsset_Font,
-    LinceAsset_Misc // Generic files
+    LinceAsset_Misc, // Generic files
+    LinceAsset_Count // Number of asset types
 };
 
-
-// Contains all the cached assets of the same type
-// Useful because all should have the same size
-struct LinceAssetStore {
-    array_t paths;
-    array_t names;
+typedef struct LinceAssetInfo {
+    // uuid?
     int type;
-    array_t data;
-    // Custom arguments for textures loading?
-    // Default asset in case requested one is not found?
+    void* handle;
+    LinceBool loaded;           ///< Whether the resource is loaded
+    char path[LINCE_PATH_MAX];  ///< Full path to the resource
+    char name[LINCE_NAME_MAX];  ///< Filename of the resource
+} LinceAssetInfo;
 
-    union asset_refs {
-        LinceTexture* texture;
-        void* misc;
-    } ref;
-
-};
-
-*/
+// Stores data for assets of the same type
+typedef struct LinceAssetStore {
+    int type;
+    array_t info;            ///<
+    hashmap_t info_indices;  ///< 
+    hashmap_t handles; ///< Maps name to index
+    // ctor & dtor functions specific to this type
+} LinceAssetStore;
 
 typedef struct LinceAssetCache {
-    char exe_dir[LINCE_PATH_MAX];   ///< Directory where the executable is located
-    array_t asset_folders;          ///< Array of relative paths between executable and asset folders
+    char exedir[LINCE_PATH_MAX];     ///< Directory where the executable is located
+    size_t exedir_length;
     char result_path[LINCE_PATH_MAX]; ///< Stores result of LinceFetchAssetPath
-
-    // Maps resource name (e.g. "textures/player.png") to an asset pointer.
-    // If it's not loaded, it loads it into memory and caches it.
-    // hashmap_t name_map;
-
-    // Maps an asset type name to its enum value,
-    // e.g. "textures" -> LinceAssetType_Texture
-    // Created at initialisation
-    // hashmap_t type_map;
-
-    // Stores raw asset data
-    // array<LinceAssetStore>
-    // e.g. textures_store = stores[LinceAssetType_Texture];
-    // array_t stores;        
-
+    array_t folders;            ///< Array of relative paths between executable and asset folders
+    array_t stores; /**< Stores asset info and handles, indexed by enum LinceAssetType */
+    
 } LinceAssetCache;
 
 /*
@@ -63,33 +50,17 @@ cache = LinceAssetCacheInit()
 LinceAssetCachePushFolder(cache, "path_to_my_assets_folder");
 LinceAssetCachePushFolder(cache, "path_to_my_second_assets_folder");
 
-LinceTexture* player = LinceAssetCacheGet(cache, "textures/player.png");
+LinceTexture* player = LinceAssetCacheGet(cache, "textures/player.png", LinceAsset_Texture);
 
 LinceAssetCacheUninit(cache);
 
-
-// Identify the type of asset from its name, e.g. "textures/player.png" is a TextureType
-static int LinceAssetCacheFindType(LinceAssetCache* cache, const char* filename);
-
 // Load an asset using a switch and its appropriate function
-static void* LinceAssetCacheLoad(LinceAssetCache* cache, const char* filename);
+void* LinceAssetCacheLoad(LinceAssetCache* cache, const char* filename, int type);
 
 // Fetch an asset. If not cached, load it.
-void* LinceAssetCacheGet(LinceAssetCache* cache, const char* name){
-    void* asset = hashmap_get(&cache->name_map, name);
-    if(!asset){
-        // Attempt to load it
-        asset = LinceAssetCacheLoad(cache, name);
-        if(!asset){
-            // Emit warning
-            return NULL;
-        }
-    }
-    return asset;
-}
+void* LinceAssetCacheGet(LinceAssetCache* cache, const char* name, int type);
 
 */
-
 
 
 /** @brief Initialise asset cache */
@@ -111,6 +82,8 @@ LinceBool LinceAssetCachePushFolder(LinceAssetCache* cache, const char* dir);
 * @note the path returned is saved internally and will be overwritten the next time the function is called.
 */
 char* LinceAssetCacheFetchPath(LinceAssetCache* cache, const char* filename);
+
+void LinceAssetCacheLoad(LinceAssetCache* cache, const char* filename, int type);
 
 
 #endif /* LINCE_ASSET_MANAGER_H */
