ca078106ea32c3145bd6541b580b0c732c5805e1
jorgefz <jorgefz.fernandez@gmail.com>
2024-04-01 18:43:21 +0100

    Added tests for asset cache

--- test/src/test_asset_cache/test_asset_cache.c
+++ test/src/test_asset_cache/test_asset_cache.c
@@ -6,36 +6,44 @@
 
 #include "lince/app/asset_cache.h"
 #include "lince/utils/fileio.h"
+#include "lince/utils/image.h"
+#include "lince/utils/memory.h"
 
 /* Verifies the asset cache is successfully initialised */
 void test_asset_cache_init(void** state){
     (void)state;
-    LinceAssetCache assets;
-    LinceInitAssetCache(&assets);
+    LinceAssetCache* cache = LinceCreateAssetCache();
 
-    assert_true(LinceIsDir(assets.exedir));
+    assert_true(LinceIsDir(cache->exedir));
+    
+    for (int i = 0; i != LinceAssetType_Count; ++i) {
+        LinceAssetStore* st =  array_get(&cache->stores, i);
+        assert_int_equal(st->type, i);
+        assert_non_null(st->handles.table);
+        assert_non_null(st->callbacks.load);
+        assert_non_null(st->callbacks.unload);
+    }
 
-    LinceUninitAssetCache(&assets);
+    LinceDeleteAssetCache(cache);
 }
 
 /* Verifies adding an existing asset folder is successful */
 void test_asset_cache_push_folder_real(void** state){
     (void)state;
-    LinceAssetCache assets;
-    LinceInitAssetCache(&assets);
+    LinceAssetCache* cache = LinceCreateAssetCache();
 
-    int success;
+    LinceBool success;
     const char folder[] =  "../../../lince/assets/";
-    size_t exe_path_len = strlen(assets.exedir);
-    success = LinceAssetCachePushFolder(&assets, folder);
+    size_t exe_path_len = strlen(cache->exedir);
+    success = LinceAssetCachePushFolder(cache, folder);
     assert_int_equal(success, 1);
-    assert_int_equal(assets.folders.size, 1);
+    assert_int_equal(cache->folders.size, 1);
 
     // First part of asset folder is path to executable
-    char* result = array_get(&assets.folders, 0);
+    char* result = array_get(&cache->folders, 0);
     assert_memory_equal(
         result,
-        assets.exedir,
+        cache->exedir,
         exe_path_len
     );
     // Second part of asset folder is custom path pushed earlier
@@ -45,47 +53,45 @@ void test_asset_cache_push_folder_real(void** state){
         sizeof(folder)
     );
 
-    LinceUninitAssetCache(&assets);
+    LinceDeleteAssetCache(cache);
 }
 
 
 /* Verifies adding an non-existent asset folder is unsuccessful */
 void test_asset_cache_push_folder_fake(void** state){
     (void)state;
-    LinceAssetCache assets;
-    LinceInitAssetCache(&assets);
+    LinceAssetCache* cache = LinceCreateAssetCache();
 
     int success;
     const char folder[] =  "FAKE/";
-    success = LinceAssetCachePushFolder(&assets, folder);
+    success = LinceAssetCachePushFolder(cache, folder);
 
     assert_int_equal(success, 0);
-    assert_int_equal(assets.folders.size, 0);
+    assert_int_equal(cache->folders.size, 0);
 
-    LinceUninitAssetCache(&assets);
+    LinceDeleteAssetCache(cache);
 }
 
 
 /* Verifies the path to a real asset can be sought successfully */
 void test_asset_cache_fetch_path_real(void** state){
     (void)state;
-    LinceAssetCache assets;
-    LinceInitAssetCache(&assets);
+    LinceAssetCache* cache = LinceCreateAssetCache();
 
     const char folder[] =  "../../../lince/assets/";
-    int success = LinceAssetCachePushFolder(&assets, folder);
+    int success = LinceAssetCachePushFolder(cache, folder);
     assert_int_equal(success, 1);
 
     const char asset[] = "fonts/DroidSans.ttf";
-    char* result = LinceAssetCacheFetchPath(&assets, asset);
+    char* result = LinceAssetCacheFetchPath(cache, asset);
     assert_non_null(result);
     assert_true(LinceIsFile(result));
 
     // Ensure path to asset was built successfully
-    size_t exe_len = strlen(assets.exedir);
+    size_t exe_len = strlen(cache->exedir);
     assert_memory_equal(
         result,
-        assets.exedir,
+        cache->exedir,
         exe_len
     );
     assert_memory_equal(
@@ -99,52 +105,73 @@ void test_asset_cache_fetch_path_real(void** state){
         sizeof(asset)
     );
 
-    LinceUninitAssetCache(&assets);
+    LinceDeleteAssetCache(cache);
 }
 
 /* Verifies the path to a non-exsitent asset is correctly flagged as such */
 void test_asset_cache_fetch_path_fake(void** state){
     (void)state;
-    LinceAssetCache assets;
-    LinceInitAssetCache(&assets);
+    LinceAssetCache* cache = LinceCreateAssetCache();
 
     const char folder[] =  "../../../lince/assets/";
-    int success = LinceAssetCachePushFolder(&assets, folder);
+    int success = LinceAssetCachePushFolder(cache, folder);
     assert_int_equal(success, 1);
 
     const char asset[] = "FAKE";
-    char* result = LinceAssetCacheFetchPath(&assets, asset);
+    char* result = LinceAssetCacheFetchPath(cache, asset);
     assert_null(result);
 
-    LinceUninitAssetCache(&assets);
+    LinceDeleteAssetCache(cache);
 }
 
 void test_asset_cache_asset_shadowing(void** state){
     (void)state;
-    LinceAssetCache assets;
-    LinceInitAssetCache(&assets);
+    LinceAssetCache* cache = LinceCreateAssetCache();
 
     int success;
     const char folder1[] =  "../../../lince/assets/";
-    success = LinceAssetCachePushFolder(&assets, folder1);
+    success = LinceAssetCachePushFolder(cache, folder1);
     assert_int_equal(success, 1);
 
     const char folder2[] =  "../../../test/assets/";
-    success = LinceAssetCachePushFolder(&assets, folder2);
+    success = LinceAssetCachePushFolder(cache, folder2);
     assert_int_equal(success, 1);
 
     const char asset[] = "fonts/DroidSans.ttf";
-    char* result = LinceAssetCacheFetchPath(&assets, asset);
+    char* result = LinceAssetCacheFetchPath(cache, asset);
     assert_non_null(result);
     assert_true(LinceIsFile(result));
 
     // Ensure folder 2 overshadows folder 1
-    size_t exe_len = strlen(assets.exedir);
+    size_t exe_len = strlen(cache->exedir);
     assert_memory_equal(
         result + exe_len,
         folder2,
         sizeof(folder2)-1 // exclude terminator
     );
 
-    LinceUninitAssetCache(&assets);
+    LinceDeleteAssetCache(cache);
+}
+
+
+/* Adds an asset to an existing cache */
+void test_asset_cache_add(void** state){
+    (void)state;
+    LinceAssetCache* cache = LinceCreateAssetCache();
+
+    LinceImage* img = LinceMalloc(sizeof(LinceImage));
+    uint8_t img_data[] = {0x1, 0x2, 0x3, 0x4};
+    img->height = 1;
+    img->width  = 1;
+    img->channels = 4;
+    img->data = LinceNewCopy(img_data, sizeof(img_data));  ;
+
+    void* retval = LinceAssetCacheAdd(cache, LinceAssetType_Image, "image", img);
+    
+    assert_ptr_equal(retval, img);
+    LinceAssetStore* st = array_get(&cache->stores, LinceAssetType_Image);
+    assert_int_equal(hashmap_has_key(&st->handles, "image"), 1);
+    assert_ptr_equal(hashmap_get(&st->handles, "image"), img);
+    
+    LinceDeleteAssetCache(cache);
 }
--- test/src/test_asset_cache/test_asset_cache.h
+++ test/src/test_asset_cache/test_asset_cache.h
@@ -7,7 +7,8 @@
     cmocka_unit_test(test_asset_cache_push_folder_fake), \
     cmocka_unit_test(test_asset_cache_fetch_path_real), \
     cmocka_unit_test(test_asset_cache_fetch_path_fake), \
-    cmocka_unit_test(test_asset_cache_asset_shadowing)
+    cmocka_unit_test(test_asset_cache_asset_shadowing), \
+    cmocka_unit_test(test_asset_cache_add)
     
 
 
@@ -28,5 +29,8 @@ void test_asset_cache_fetch_path_fake(void** state);
 
 /* Verifies any existing engine asset can be shadowed by another asset named identically */
 void test_asset_cache_asset_shadowing(void** state);
+
+/* Adds an asset to an existing cache */
+void test_asset_cache_add(void** state);
 
 #endif /* TEST_ASSET_CACHE */
