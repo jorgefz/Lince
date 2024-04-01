878488143ec48a93f824695de785cd7c868c9249
jorgefz <jorgefz.fernandez@gmail.com>
2024-04-01 18:43:08 +0100

    Added asset stores and caching via separate stores for each resource type

--- lince/src/lince/app/asset_cache.c
+++ lince/src/lince/app/asset_cache.c
@@ -1,30 +1,127 @@
 #include "lince/app/asset_cache.h"
 #include "lince/utils/fileio.h"
 #include "lince/core/logger.h"
+#include "lince/utils/memory.h"
 
-LinceBool LinceInitAssetCache(LinceAssetCache* cache){
+#include "lince/utils/image.h"
+#include "lince/renderer/texture.h"
+
+
+/* ============== STATIC VARIABLES ============== */
+
+
+
+
+/* ============== STATIC FUNCTION DECLARATIONS ============== */
+
+/** @brief Initialise asset cache
+ * The user is forced to use `LinceCreateAssetCache` because LinceAssetCache is large
+ * and it's a bad idea to keep it in the stack.
+ * @returns LinceTrue if successful, and LinceFalse otherwise
+ */
+static LinceBool LinceInitAssetCache(LinceAssetCache* cache);
+
+/** @brief Uninitialise asset cache, freeing internally allocated memory */
+static void LinceUninitAssetCache(LinceAssetCache* cache);
+
+static void* LinceAssetCacheLoadImage(const char* path);
+static void LinceAssetCacheUnloadImage(void* handle);
+
+static void* LinceAssetCacheLoadTexture(const char* path);
+static void LinceAssetCacheUnloadTexture(void* handle);
+
+
+static const LinceAssetCallbacks _asset_callbacks[LinceAssetType_Count] = {
+        [LinceAssetType_Image] = {LinceAssetCacheLoadImage, LinceAssetCacheUnloadImage},
+        [LinceAssetType_Texture] = {LinceAssetCacheLoadTexture, LinceAssetCacheUnloadTexture}
+};
+
+
+
+/* ============== STATIC FUNCTION DEFINITIONS ============== */
+
+
+static LinceBool LinceInitAssetCache(LinceAssetCache* cache) {
     cache->exedir_length = LinceFetchExeDir(cache->exedir, LINCE_PATH_MAX);
-    if(cache->exedir_length == 0){
+    if (cache->exedir_length == 0) {
         return LinceFalse;
     }
 
     array_init(&cache->folders, LINCE_PATH_MAX * sizeof(char));
-    array_init(&cache->stores, LinceAsset_Count * sizeof(hashmap_t));
-    for(hashmap_t* map = cache->stores.begin; map != cache->stores.end; ++map){
-        hashmap_init(map, 10);
+
+    // Initialise stores
+    array_init(&cache->stores, sizeof(LinceAssetStore));
+    array_resize(&cache->stores, LinceAssetType_Count);
+    for (uint32_t i = 0; i != LinceAssetType_Count; ++i) {
+        LinceAssetStore* st = array_get(&cache->stores, i);
+        st->type = (int)i;
+        st->callbacks = _asset_callbacks[i];
+        hashmap_init(&st->handles, 10);
     }
 
     return LinceTrue;
 }
 
-void LinceUninitAssetCache(LinceAssetCache* cache){
+
+static void LinceUninitAssetCache(LinceAssetCache* cache) {
     array_uninit(&cache->folders);
-    for(hashmap_t* map = cache->stores.begin; map != cache->stores.end; ++map){
-        hashmap_uninit(map);
+    for (int i = 0; i != LinceAssetType_Count; ++i) {
+        LinceAssetStore* st = array_get(&cache->stores, i);
+        char* key = NULL;
+        while ((key = hashmap_iter(&st->handles, key))) {
+            LinceAssetCacheUnload(cache, key, i);
+        }
+        hashmap_uninit(&st->handles);
     }
     array_uninit(&cache->stores);
-
 }
+
+
+static void* LinceAssetCacheLoadImage(const char* path) {
+    LinceImage* handle = LinceMalloc(sizeof(LinceImage));
+    void* ret = LinceLoadImage(handle, path);
+    if (!ret) {
+        LinceFree(handle);
+        return NULL;
+    }
+    return handle;
+}
+
+static void LinceAssetCacheUnloadImage(void* handle) {
+    LinceDeleteImage(handle);
+    LinceFree(handle);
+}
+
+static void* LinceAssetCacheLoadTexture(const char* path) {
+    return LinceCreateTextureFromFile(path, 0);
+}
+
+static void LinceAssetCacheUnloadTexture(void* handle) {
+    LinceDeleteTexture(handle);
+}
+
+
+
+/* ============== PUBLIC FUNCTION DEFINITIONS ============== */
+
+LinceAssetCache* LinceCreateAssetCache() {
+    LinceAssetCache* cache = LinceMalloc(sizeof(LinceAssetCache));
+    if (!cache) return NULL;
+
+    if (!LinceInitAssetCache(cache)) {
+        LinceFree(cache);
+        return NULL;
+    }
+    return cache;
+}
+
+void LinceDeleteAssetCache(LinceAssetCache* cache) {
+    LinceUninitAssetCache(cache);
+    LinceFree(cache);
+}
+
+
+
 
 LinceBool LinceAssetCachePushFolder(LinceAssetCache* cache, const char* folder){
     size_t folder_len = strlen(folder);
@@ -89,24 +186,45 @@ char* LinceAssetCacheFetchPath(LinceAssetCache* cache, const char* asset_filenam
     return NULL;
 }
 
-/*
-void LinceAssetCacheLoad(LinceAssetCache* cache, const char* filename, int type){
-
-    // Build full path
-    // path = cache->exedir + asset_folder + filename
-
-    void* handle;
-    char* path;
-
-    switch(type){
-        case LinceAsset_Texture:
-            handle = LinceLoadTexture(path, 0);
-            break;
-        case LinceAsset_Shader:
-            handle = LinceLoadShader(path, headers);
-        case LinceAsset_Font:
-            handle = LinceUILoadFonts(path, sizes);
-    }
-
+void* LinceAssetCacheAdd(LinceAssetCache* cache, int type, const char* name, void* handle){
+    if(!handle) return NULL;
+    if (type < 0 || type >= LinceAssetType_Count) return NULL;
+    
+    LinceAssetStore* st = array_get(&cache->stores, type);
+    hashmap_set(&st->handles, name, handle);
+    return handle;
 }
-*/
+
+
+void* LinceAssetCacheGet(LinceAssetCache* cache, const char* name, int type){
+    if(type < 0 || type >= LinceAssetType_Count) return NULL;
+
+    LinceAssetStore* st = array_get(&cache->stores, type);
+    hashmap_t* handles = &st->handles;
+    void* handle = hashmap_get(handles, name);
+    if(handle) return handle;
+
+    return LinceAssetCacheLoad(cache, name, type);
+}
+
+
+void* LinceAssetCacheLoad(LinceAssetCache* cache, const char* name, int type){
+    if (type < 0 || type >= LinceAssetType_Count) return NULL;
+    
+    char* path = LinceAssetCacheFetchPath(cache, name);
+    if(!path) return NULL;
+
+    LinceAssetStore* st = array_get(&cache->stores, type);
+    void* handle = st->callbacks.load(path);
+    hashmap_set(&st->handles, name, handle);
+    return handle;
+}
+
+void LinceAssetCacheUnload(LinceAssetCache* cache, const char* name, int type){
+    if (type < 0 || type >= LinceAssetType_Count) return;
+
+    LinceAssetStore* st = array_get(&cache->stores, type);
+    void* handle = hashmap_get(&st->handles, name);
+    st->callbacks.unload(handle);
+    hashmap_set(&st->handles, name, NULL);
+}
--- lince/src/lince/app/asset_cache.h
+++ lince/src/lince/app/asset_cache.h
@@ -5,36 +5,66 @@
 #include "lince/containers/array.h"
 #include "lince/containers/hashmap.h"
 
-enum LinceAssetType {
-    LinceAsset_None = -1,
-    LinceAsset_Texture = 0,
-    // LinceAsset_Image,
-    // LinceAsset_TextFile,
-    // LinceAsset_ShaderHeader,
-    // LinceAsset_ShaderProgram,
-    // LinceAsset_Font,
+
+typedef enum LinceAssetType {
+    LinceAssetType_None = -1,
+    LinceAssetType_Image = 0, // p = Load(p, path)
+    LinceAssetType_Texture,   // p = Load(path, flags) 
+    // LinceAsset_TextFile,   // p = Load(path);
+    // LinceAsset_ShaderHeader,  // 
+    // LinceAsset_ShaderProgram, // p = Load(path1, path2)
+    // LinceAsset_Font,          // p = Load(path, sizes[])
     // LinceAsset_Misc, // Generic files
-    LinceAsset_Count // Number of asset types
-};
+    LinceAssetType_Count // Number of asset types
+} LinceAssetType;
+
+
+///< Callback signature to load an asset from disk
+typedef void* (*LinceAssetOnLoad)(const char* path);
+
+///< Callback signature to free an asset from memory
+typedef void (*LinceAssetOnUnload)(void* obj);
+
+/// Callbacks to load, unload, and manage assets of a specific type
+typedef struct LinceAssetCallbacks {
+    LinceAssetOnLoad   load;   ///< Callback to load an asset from disk
+    LinceAssetOnUnload unload; ///< Callback to free an asset from memory
+} LinceAssetCallbacks;
+
+/// Stores assets of a single type
+typedef struct LinceAssetStore {
+    int type;
+    LinceAssetCallbacks callbacks;
+    hashmap_t handles;
+} LinceAssetStore;
+
 
 typedef struct LinceAssetCache {
-    char exedir[LINCE_PATH_MAX];     ///< Directory where the executable is located
-    size_t exedir_length;
+    char exedir[LINCE_PATH_MAX];      ///< Directory where the executable is located
+    size_t exedir_length;             ///< Number of characters in the path of the executable
     char result_path[LINCE_PATH_MAX]; ///< Stores result of LinceFetchAssetPath
-    array_t folders;  ///< Array of relative paths between executable and asset folders
-    array_t stores;   ///< array<hashmap> Stores info and handles for each asset type, indexed by LinceAssetType
+
+    /// Array of relative paths between the executable and the asset folders
+    array_t folders;
     
+    /// Stores info and handles for each asset type
+    array_t stores; // array<LinceAssetStore> Cache stores for different asset types
+    // LinceAssetStore stores[LinceAssetType_Count];
+
 } LinceAssetCache;
 
 
+/** @brief Create an asset cache
+ * @returns Valid pointer to initialised asset cache, or NULL if unsuccessful.
+ */
+LinceAssetCache* LinceCreateAssetCache();
 
-/** @brief Initialise asset cache
- * @returns LinceTrue if successful, and LinceFalse otherwise
-*/
-LinceBool LinceInitAssetCache(LinceAssetCache* cache);
+/** @brief Deletes an asset cache
+ * Unloads and frees all assets it holds.
+ * @param cache Cache to delete. Must be the return value of `LinceCreateAssetCache`.
+ */
+void LinceDeleteAssetCache(LinceAssetCache* cache);
 
-/** @brief Uninitialise asset cache, freeing internally allocated memory */
-void LinceUninitAssetCache(LinceAssetCache* cache);
 
 /** @brief Saves the location of an asset folder to the list of search paths.
 * The path must be relative to the location of the executable.
@@ -50,7 +80,22 @@ LinceBool LinceAssetCachePushFolder(LinceAssetCache* cache, const char* folder);
 */
 char* LinceAssetCacheFetchPath(LinceAssetCache* cache, const char* filename);
 
-void LinceAssetCacheLoad(LinceAssetCache* cache, const char* filename, int type);
+/** @brief Adds an existing asset to the cache
+ * The asset must be heap-allocated. Passing it to the cache
+ * will mean transfering ownership to it.
+*/
+void* LinceAssetCacheAdd(LinceAssetCache* cache, int type, const char* name, void* handle);
+
+/** @brief Load an asset from memory */
+void* LinceAssetCacheLoad(LinceAssetCache* cache, const char* name, int type);
+
+/** @brief Unload a cached asset */
+void LinceAssetCacheUnload(LinceAssetCache* cache, const char* name, int type);
+
+/** @brief Retrieve a cached asset */
+void* LinceAssetCacheGet(LinceAssetCache* cache, const char* name, int type);
+
+// void* LinceAssetCacheLoadAsync(LinceAssetCache* cache, const char* name, int type);
 
 
 #endif /* LINCE_ASSET_MANAGER_H */
