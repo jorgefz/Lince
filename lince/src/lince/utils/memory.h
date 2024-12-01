18ade3d2584bce0c5c517f46b8a7103d9308abb0
jorgefz <jorgefz.fernandez@gmail.com>
2024-12-01 08:55:09 +0000

    Removed old memory allocation functions (#11)

--- lince/src/lince/utils/memory.h
+++ lince/src/lince/utils/memory.h
@@ -7,20 +7,5 @@
 
 #include "lince/core/core.h"
 
-/** @brief `malloc` with checks and assertion of return pointer */
-// void* LinceAlloc(size_t nbytes);
-
-/** @brief `calloc` with checks and assertion of return pointer */
-// void* LinceCalloc(size_t nbytes);
-// #define LinceCalloc(SZ) memset(LinceAlloc(SZ), (char)0, (SZ));
-
-/** @brief `realloc` with checks and assertion of return pointer */
-// void* LinceRealloc(void* ptr, size_t nbytes);
-
-/** @brief Frees memory and sets provided pointer to NULL */
-// #define LinceFree(p) do{ if(p) LINCE_FREE(p); (p) = NULL; } while(0)
-
-/** @brief Copies `nbytes` from `ptr` into heap-allocated memory */
-// void* LinceNewCopy(const void* ptr, size_t nbytes);
 
 #endif /* LINCE_MEMORY_H */
